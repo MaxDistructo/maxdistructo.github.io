6119e52cf92ca9a154d07b470a8eaa688fc1a88b
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-03-29 09:42:59 -0500

    Create forge1.10download.sh

---
+++ forge1.10download.sh
@@ -0,0 +1,5 @@
+#bin/bash
+wget https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.10.2-12.18.3.2185/forge-1.10.2-12.18.3.2185-mdk.zip
+unzip forge-1.10.2-12.18.3.2185-mdk.zip
+chmod 755 gradlew
+./gradlew buildDecompWorkspace
