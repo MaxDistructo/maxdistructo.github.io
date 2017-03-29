c2c5e599acd529d7431f27b9f9774143f70ccb3a
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-03-29 09:44:12 -0500

    Update forge1.10download.sh

--- forge1.10download.sh
+++ forge1.10download.sh
@@ -1,4 +1,4 @@
-#bin/bash
+#!/bin/bash
 wget https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.10.2-12.18.3.2185/forge-1.10.2-12.18.3.2185-mdk.zip
 unzip forge-1.10.2-12.18.3.2185-mdk.zip
 chmod 755 gradlew
