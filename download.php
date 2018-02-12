2af1715f9edb2cec52063d78d25b64801819d148
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-02-12 08:53:57 -0600

    Update download.php

--- download.php
+++ download.php
@@ -1,14 +1,21 @@
-function _Download($f_location, $f_name){
-  $file = uniqid() . '.pdf';
-
-  file_put_contents($file,file_get_contents($f_location));
-
-  header('Content-Description: File Transfer');
-  header('Content-Type: application/octet-stream');
-  header('Content-Length: ' . filesize($file));
-  header('Content-Disposition: attachment; filename=' . basename($f_name));
-
-  readfile($file);
+<?php
+if(isset($_REQUEST["file"])){
+    // Get parameters
+    $file = urldecode($_REQUEST["file"]); // Decode URL-encoded string
+    $filepath = "images/" . $file;
+    
+    // Process download
+    if(file_exists($filepath)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($filepath));
+        flush(); // Flush system output buffer
+        readfile($filepath);
+        exit;
+    }
 }
-
-_Download($_GET['file'], "file.pdf");
+?>
